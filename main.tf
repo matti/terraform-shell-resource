d6f3bf887e05a1ea3fbf55949aa745c573fb485a
Matti Paksula <matti.paksula@iki.fi>
2018-04-16 14:54:49 +0300

    chore (dependencies) Removes the need for ruby / external resource.

--- main.tf
+++ main.tf
@@ -9,15 +9,32 @@ locals {
   command_when_destroy_chomped = "${chomp(var.command_when_destroy)}"
 }
 
+# these provide an empty file for data sources to read without exploding
+resource "local_file" "stdout" {
+  content  = ""
+  filename = "${path.module}/stdout.${null_resource.start.id}"
+}
+
+resource "local_file" "stderr" {
+  content  = ""
+  filename = "${path.module}/stderr.${null_resource.start.id}"
+}
+
+resource "local_file" "exitstatus" {
+  content  = ""
+  filename = "${path.module}/exitstatus.${null_resource.start.id}"
+}
+
+# this overwrites local_files
 resource "null_resource" "shell" {
-  depends_on = ["null_resource.start"]
+  depends_on = ["local_file.stdout", "local_file.stderr", "local_file.exitstatus"]
 
   triggers = {
     string = "${var.trigger}"
   }
 
   provisioner "local-exec" {
-    command = "${local.command_chomped} 2>\"${path.module}/stderr.${self.id}\" >\"${path.module}/stdout.${self.id}\"; echo $? >\"${path.module}/exitstatus.${self.id}\""
+    command = "${local.command_chomped} 2>\"${path.module}/stderr.${null_resource.start.id}\" >\"${path.module}/stdout.${null_resource.start.id}\"; echo $? >\"${path.module}/exitstatus.${null_resource.start.id}\""
   }
 
   provisioner "local-exec" {
@@ -27,38 +44,45 @@ resource "null_resource" "shell" {
 
   provisioner "local-exec" {
     when       = "destroy"
-    command    = "rm \"${path.module}/stdout.${self.id}\""
+    command    = "rm \"${path.module}/stdout.${null_resource.start.id}\""
     on_failure = "continue"
   }
 
   provisioner "local-exec" {
     when       = "destroy"
-    command    = "rm \"${path.module}/stderr.${self.id}\""
+    command    = "rm \"${path.module}/stderr.${null_resource.start.id}\""
     on_failure = "continue"
   }
 
   provisioner "local-exec" {
     when       = "destroy"
-    command    = "rm \"${path.module}/exitstatus.${self.id}\""
+    command    = "rm \"${path.module}/exitstatus.${null_resource.start.id}\""
     on_failure = "continue"
   }
 }
 
-data "external" "read" {
-  program = ["ruby", "${path.module}/read.rb"]
-
-  query = {
-    stdout     = "${path.module}/stdout.${null_resource.shell.id}"
-    stderr     = "${path.module}/stderr.${null_resource.shell.id}"
-    exitstatus = "${path.module}/exitstatus.${null_resource.shell.id}"
-  }
+# on the first apply these will get the overridden contents
+data "local_file" "stdout" {
+  filename   = "${path.module}/stdout.${null_resource.start.id}"
+  depends_on = ["null_resource.shell", "local_file.stdout"]
 }
 
+data "local_file" "stderr" {
+  filename   = "${path.module}/stderr.${null_resource.start.id}"
+  depends_on = ["null_resource.shell", "local_file.stderr"]
+}
+
+data "local_file" "exitstatus" {
+  filename   = "${path.module}/exitstatus.${null_resource.start.id}"
+  depends_on = ["null_resource.shell", "local_file.exitstatus"]
+}
+
+# first apply stores contents and then ignores the later empty contents
 resource "null_resource" "contents" {
   triggers = {
-    stdout     = "${data.external.read.result["stdout"]}"
-    stderr     = "${data.external.read.result["stderr"]}"
-    exitstatus = "${data.external.read.result["exitstatus"]}"
+    stdout     = "${data.local_file.stdout.content}"
+    stderr     = "${data.local_file.stderr.content}"
+    exitstatus = "${data.local_file.exitstatus.content}"
   }
 
   lifecycle {
