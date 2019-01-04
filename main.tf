fd139bb24dee26b4476b5141cf8ec713a0ab76e6
Matti Paksula <matti.paksula@iki.fi>
2019-01-05 01:05:58 +0200

    fix (apply) running apply again works.

--- main.tf
+++ main.tf
@@ -9,26 +9,7 @@ locals {
   command_when_destroy_chomped = "${chomp(var.command_when_destroy)}"
 }
 
-# these provide an empty file for data sources to read without exploding
-resource "local_file" "stdout" {
-  content  = ""
-  filename = "${path.module}/stdout.${null_resource.start.id}"
-}
-
-resource "local_file" "stderr" {
-  content  = ""
-  filename = "${path.module}/stderr.${null_resource.start.id}"
-}
-
-resource "local_file" "exitstatus" {
-  content  = ""
-  filename = "${path.module}/exitstatus.${null_resource.start.id}"
-}
-
-# this overwrites local_files
 resource "null_resource" "shell" {
-  depends_on = ["local_file.stdout", "local_file.stderr", "local_file.exitstatus"]
-
   triggers = {
     string = "${var.trigger}"
   }
@@ -61,28 +42,13 @@ resource "null_resource" "shell" {
   }
 }
 
-# on the first apply these will get the overridden contents
-data "local_file" "stdout" {
-  filename   = "${path.module}/stdout.${null_resource.start.id}"
-  depends_on = ["null_resource.shell", "local_file.stdout"]
-}
-
-data "local_file" "stderr" {
-  filename   = "${path.module}/stderr.${null_resource.start.id}"
-  depends_on = ["null_resource.shell", "local_file.stderr"]
-}
-
-data "local_file" "exitstatus" {
-  filename   = "${path.module}/exitstatus.${null_resource.start.id}"
-  depends_on = ["null_resource.shell", "local_file.exitstatus"]
-}
-
-# first apply stores contents and then ignores the later empty contents
 resource "null_resource" "contents" {
+  depends_on = ["null_resource.shell"]
+
   triggers = {
-    stdout     = "${data.local_file.stdout.content}"
-    stderr     = "${data.local_file.stderr.content}"
-    exitstatus = "${data.local_file.exitstatus.content}"
+    stdout     = "${file("${path.module}/stdout.${null_resource.start.id}")}"
+    stderr     = "${file("${path.module}/stderr.${null_resource.start.id}")}"
+    exitstatus = "${file("${path.module}/exitstatus.${null_resource.start.id}")}"
   }
 
   lifecycle {
